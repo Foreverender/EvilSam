f4c7305120a91c6efb42848e1851141e9c4d2838
Foreverender <87213367+Foreverender@users.noreply.github.com>
2021-08-04 14:51:19 -0400

    Update Scene3.js

--- Scene3.js
+++ Scene3.js
@@ -36,7 +36,8 @@ class Scene3 extends Phaser.Scene {
                      "Sometimes your dumbest ideas are the best ones.",
                      "I want a story about that corn cob. How did she die? What was her life like? What is her name?",
                      "Do you think God stays in heaven because he, too, lives in fear of what he's created here on earth?",
-                     "To be a real Neapolitan pizza it has to have chocolate, vanilla, and strawberry on it."];
+                     "To be a real Neapolitan pizza it has to have chocolate, vanilla, and strawberry on it.",
+                     "The harder you work for something, the more tired you will be."];
 
     var num = Math.floor(Math.random() * Math.floor(blessings.length));
 
