68ccab7ee60b4fdf3a85e25bb6774e3cba1d5993
Foreverender <87213367+Foreverender@users.noreply.github.com>
2021-10-06 12:59:40 -0400

    Update Scene3.js

--- Scene3.js
+++ Scene3.js
@@ -40,7 +40,8 @@ class Scene3 extends Phaser.Scene {
                      "The harder you work for something, the more tired you will be.",
                      "The resolve to make even the smallest decision can lead to greater mistakes!",
                      "We accept the karma that we think we deserve.",
-                     "Did you guys hear about the new law in Hawaii they passed? Due to noise complaints youre no longer allowed to laugh above a certain decibel...\n \n \n now you can only use a low ha"];
+                     "Did you guys hear about the new law in Hawaii they passed? Due to noise complaints youre no longer allowed to laugh above a certain decibel...\n \n \n now you can only use a low ha",
+                     "Fight viciously over the scraps and crumbs that life allows you."];
 
     var num = Math.floor(Math.random() * Math.floor(blessings.length));
 
