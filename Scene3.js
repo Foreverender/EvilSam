d3467a78c2aa1938be7cb7fc616a95c7ffcd2566
Foreverender <87213367+Foreverender@users.noreply.github.com>
2021-11-30 20:54:21 -0500

    Update Scene3.js

--- Scene3.js
+++ Scene3.js
@@ -12,7 +12,7 @@ class Scene3 extends Phaser.Scene {
     const samspeaks = this.add.sprite(config.width - 225, config.height - 225, "samtalk");
     samspeaks.setScale(3);
     
-    this.samhat = this.add.image(560, 376, "samhat");
+    this.samhat = this.add.image(560, 378, "samhat");
     this.samhat.setScale(3);
 
     const scrollmove = this.add.sprite(config.width - 550, config.height - 75, "openscroll");
